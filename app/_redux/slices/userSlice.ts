00f7c9a5949a9989b370e777ddad0456555a0933
anthony <antle9053@gmail.com>
2024-07-02 22:59:05 +0700

    Auth slice

---
+++ app/_redux/slices/userSlice.ts
@@ -0,0 +1,37 @@
+import { createSlice } from "@reduxjs/toolkit";
+import type { PayloadAction } from "@reduxjs/toolkit";
+import { User } from "@supabase/auth-helpers-nextjs";
+import Stripe from "stripe";
+
+interface UserDetails {
+  id: string;
+  first_name: string;
+  last_name: string;
+  full_name?: string;
+  avatar_url?: string;
+  billing_address?: Stripe.Address;
+  payment_method?: Stripe.PaymentMethod[Stripe.PaymentMethod.Type];
+}
+
+interface UserState {
+  accessToken: string | null;
+  user: User | null;
+  userDetails: UserDetails | null;
+  isLoading: boolean;
+}
+
+const initialState = {
+  accessToken: null,
+  user: null,
+  userDetails: null,
+  isLoading: false,
+} satisfies UserState as UserState;
+
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {},
+});
+
+export const {} = userSlice.actions;
+export default userSlice.reducer;
